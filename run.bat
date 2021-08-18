bf6f87aa7bac7ba3e49f287c25759cf0d50827f0
goson535 <sig85@bk.ru>
2021-08-19 00:22:14 +0300

    9

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 set "d=C:\Users\Denis\Documents\GitHub\fast2"
-for /f "delims=" %%a in ('dir/a-d/b/s "%d%\*.html"') do ren "%%a" "%%~na.htmlll"
+for /f "delims=" %%a in ('dir/a-d/b/s "%d%\*.htmlll"') do ren "%%a" "%%~na.html"
 exit
